c6ea3c450206ecba824f7967ca542fca0f4daa55
Антон <live.stone.anton@gmail.com>
2019-12-07 00:09:21 +0500

    +wev

---
+++ WebApp/Controllers/ValuesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using Viber.Bot;
+
+namespace WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // POST 
+        [HttpPost]
+        public string Post()
+        {
+            using (var reader = new StreamReader(Request.Body))
+                return reader.ReadToEnd();
+        }
+
+    }
+}
