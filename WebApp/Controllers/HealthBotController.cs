a418f832c820b3f3c59065d32aa775e87e5d5850
Антон <live.stone.anton@gmail.com>
2020-02-03 11:11:49 +0500

    32131

--- WebApp/Controllers/HealthBotController.cs
+++ WebApp/Controllers/HealthBotController.cs
@@ -12,7 +12,7 @@ namespace WebApp.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class HealthBotController : ControllerBase
     {
         //string reqTxt = "{\"event\":\"message\",\"timestamp\":1577087362822,\"chat_hostname\":\"SN-CHAT-01_\",\"message_token\":5389804735894462827,\"sender\":{\"id\":\"xp8uiNXBuhVQ3nTbdQlBYw==\",\"name\":\"Anton\",\"language\":\"ru\",\"country\":\"RU\",\"api_version\":7},\"message\":{\"text\":\"\u0437\u0430\u043F\u0438\u0448\u0438 \u043C\u043E\u0438 \u043F\u043E\u043A\u0430\u0437\u0430\u043D\u0438\u044F\",\"type\":\"text\"},\"silent\":false}";
         //string reqPhoto = "{\"event\": \"message\",\"timestamp\": 1577270319525,\"chat_hostname\": \"SN-CHAT-01_\",\"message_token\": 5390572111929636000,\"sender\": {\"id\": \"xp8uiNXBuhVQ3nTbdQlBYw==\",\"name\": \"Anton\",\"language\": \"ru\",\"country\": \"RU\",\"api_version\": 7},\"message\": {\"type\": \"picture\",\"media\": \"https://sun9-11.userapi.com/c855736/v855736568/13159f/_BpcXjL_sAQ.jpg\",\"thumbnail\": \"https://dl-media.viber.com/5/media/2/short/any/sig/image/400/ad7b/f5300cc4a02f5247fca5302671405dc8d6d9ffd07f7d1e519ae610b84f1dad7b.jpg?Expires=1577273920&Signature=Il7I74PvYCCkFrQoBHYx71~NEtVya88XsNkoKfRcKJHdVRxCCLH2isNBTJ8dfNg4BKJOzm16GoR2fZDG-T8Kdd2APwrxyRsqcuIvyLUPABsVdd14YtTXyBC2kjrmwPkxTnimKzTlR5Z963In0Xzub5WY4qEiggC80rIEoY7bQmrWDbp-ath3hc59e5-unLtI9NMiAx1YpCaq6AWuvQwlwvH23BsxQKZyBkBX3g1EjFHpL4q9Sk6~h1t1dz3ScFCV41dC9TvzNbWsPZhNSyOM7MEAblSNfhY8Fq~VpGDTkCwpT8WjLDWlAzX6sdtXohjygwpg2PJwMY3urJZsPlJOVw__&Key-Pair-Id=APKAJ62UNSBCMEIPV4HA\",\"file_name\": \"0Yupg0Qgnok.jpg\",\"size\": 356135},\"silent\": false}";
